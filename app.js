8d396ae980db49fe563ecf697546f14a129a8cf4
David Naimi <d88naimi@gmail.com>
2020-04-08 05:51:07 -0700

    app.js and prompts are working need to push to htmlrender page

--- app.js
+++ app.js
@@ -9,11 +9,169 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const team = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+class Start {
+    // inquire prompt for the development team 
+    initialPrompt() {
+        return inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "managerName",
+                    message: "What is your manager's name?"
+                },
+                {
+                    type: "input",
+                    name: "managerID",
+                    message: "What is your manager's ID?"
+                },
+                {
+                    type: "input",
+                    name: "managerEmail",
+                    message: "What is your manager's Email?"
+                },
+                {
+                    type: "input",
+                    name: "managerOfficeNumber",
+                    message: "What is your manager's Office Number?"
+                },
+                {
+                    type: "list",
+                    name: "specificRoles",
+                    message: "What type of team member would you like to add?",
+                    choices: [
+                        "Intern",
+                        "Engineer",
+                        "I am done adding team members"
+                    ]
+                },
+            ])
+            .then(data => {
+                const manager = new Manager(data.managerName, data.managerID, data.managerEmail, data.managerOfficeNumber);
+                team.push(manager);
+                console.log(team);
+                console.log(data.specificRoles);
+                if (data.specificRoles === "Intern") {
+                    this.promptIntern();
+                } else if (data.specificRoles === "Engineer") {
+                    this.promptEngineer();
+                } else {
+                    this.finish();
+                }
+            });
+    }
+    promptIntern() {
+        return inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "internName",
+                    message: "What is your intern's name?"
+                },
+                {
+                    type: "input",
+                    name: "internID",
+                    message: "What is your intern's ID?"
+                },
+                {
+                    type: "input",
+                    name: "internEmail",
+                    message: "What is your intern's Email?"
+                },
+                {
+                    type: "input",
+                    name: "internSchool",
+                    message: "What is your intern's School Number?"
+                },
+                {
+                    type: "list",
+                    name: "specificRoles",
+                    message: "What type of team member would you like to add?",
+                    choices: [
+                        "Intern",
+                        "Engineer",
+                        "I am done adding team members"
+                    ]
+                },
+            ])
+            .then(data => {
+                const intern = new Intern(data.internName, data.internID, data.internEmail, data.internSchool);
+                console.log(intern);
+                team.push(intern);
+                console.log(team);
+                console.log(data.specificRoles);
+                if (data.specificRoles === "Intern") {
+                    this.promptIntern();
+                } else if (data.specificRoles === "Engineer") {
+                    this.promptEngineer();
+                } else {
+                    this.finish();
+                }
+            });
+    }
+    promptEngineer() {
+        return inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "engineerName",
+                    message: "What is your engineer's name?"
+                },
+                {
+                    type: "input",
+                    name: "engineerID",
+                    message: "What is your engineer's ID?"
+                },
+                {
+                    type: "input",
+                    name: "engineerEmail",
+                    message: "What is your engineer's Email?"
+                },
+                {
+                    type: "input",
+                    name: "engineerGithub",
+                    message: "What is your engineer's Github ?"
+                },
+                {
+                    type: "list",
+                    name: "specificRoles",
+                    message: "What type of team member would you like to add?",
+                    choices: [
+                        "Intern",
+                        "Engineer",
+                        "I am done adding team members"
+                    ]
+                },
+            ])
+            .then(data => {
+                const engineer = new Engineer(data.engineerName, data.engineerID, data.engineerEmail, data.engineerGithub);
+                console.log(engineer);
+                team.push(engineer);
+                console.log(team);
+                console.log(data.specificRoles);
+                if (data.specificRoles === "Intern") {
+                    this.promptIntern();
+                } else if (data.specificRoles === "Engineer") {
+                    this.promptEngineer();
+                } else {
+                    this.finsih();
+                }
+            });
+    }
+    finish() {
+        // pass the the teams array to htmlrender 
+        //html render passes to output folder 
+        
+    }
 
+}
+
+const test = new Start;
+test.initialPrompt();
+console.log(team);
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -33,3 +191,4 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
